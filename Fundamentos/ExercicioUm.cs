fdfa5d314185967388f63bc984987239ae963985
LUCAS ROSSI DA SILVA <lucas.rsilva116@senacsp.edu.br>
2023-05-12 16:18:05 -0300

    first commit

---
+++ Fundamentos/ExercicioUm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.Fundamentos {
+    class ExercicioUm {
+        public static void Executar() {
+
+            double nota1, nota2, nota3;
+            double media;
+
+            Console.WriteLine("Digite a primeira nota:");
+           nota1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a segunda nota:");
+            nota2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a terceira nota:");
+            nota3 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($" A média do aluno é {(nota1 + nota2 + nota3)/2}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+using CursoCSharp.Fundamentos;
+
+namespace CursoCSharp {
+    class Program {
+        static void Main(string[] args) {
+            var central = new CentralDeExercicios(new Dictionary<string, Action>() {
+                {"Primeiro Programa - Fundamentos", PrimeiroPrograma.Executar},
+                {"Variaveis e Constantes - Fundamentos",VariaveisEConstantes.Executar },
+                {"Notação Ponto - Fundamentos ",NotacaoPonto.Executar },
+                {"Lendo Dados - Fundamentos",LendoDados.Executar },
+                {"Formatando Números - Fundamentos ",FormatandoNumeros.Executar},
+                {"Exercício 1 - Fundamentos ",ExercicioUm.Executar},
+                {"Exercício 2 - Fundamentos ",ExercicioDois.Executar},
+                {"Exercício 3 - Fundamentos ",ExercicioTres.Executar},
+                {"Operadores Relacionais - Fundamentos ",OperadoresRelacionais.Executar},
+            });
+
+            central.SelecionarEExecutar();
+        }
+    }
+}
